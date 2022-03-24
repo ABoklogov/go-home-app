7463d71b79ff8a8169121ee9c7eff5cdcc9a004f
joestalin14 <74319867+ABoklogov@users.noreply.github.com>
2022-03-24 14:15:10 +0300

    добавил 123

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="star-rating">
       <span v-for="index in starLimit" :key="index" class="star star--outlined"></span>
-      <p></p>
+      <p>123</p>
       <div class="star-rating__colored" :style="ratingWidthStyle">
          <span v-for="index in starLimit" :key="index" class="star star--colored"></span>
       </div>

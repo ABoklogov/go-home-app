2ccfb1bbc9a31043b6ae4f89e6ca2ec0d4fa1740
Алексей Боклогов <kashira87@mail.ru>
2022-05-10 12:27:48 +0300

    добавляет кнопку показать еще и шапку отзывов

--- src/components/reviews/index.vue
+++ src/components/reviews/index.vue
@@ -1,26 +1,47 @@
 <template>
   <section class="reviews">
+    <div class="reviews__heading">
+      <h1 class="reviews__title">Суммарный рейтинг</h1>
+      <div class="reviews__rating">
+        <span>{{ mountOfReviews }} отзыв(ов)</span>
+        <StarRating :rating="totalRating"/>
+      </div>
+    </div>
+
     <ReviewsItem 
       v-for="review in reviews" 
       :key="review.author" 
       :review="review"
     />
+
+    <button class="reviews__show-more">Читать еще...</button>
   </section>
 </template>
 
 <script>
 import ReviewsItem from './reviews-item';
+import StarRating from '../StarRating.vue';
 
   export default {
     name: "ReviewsSection",
     components: {
       ReviewsItem,
+      StarRating,
     },
     props: {
       reviews: {
         type: Array,
         required: true
       }
+    },
+    computed: {
+      mountOfReviews() {
+        return this.reviews.length;
+      },
+      totalRating() {
+        const total = this.reviews.reduce((acc, el) => acc + el.rating, 0);
+        return total / this.mountOfReviews;
+      },
     }
   }
 </script>
